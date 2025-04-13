61365c8cb96f20acdeaec70f193f3e00b851f19b
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-13 23:46:32 +0200

    Sorting out the sqlalchemy expecting postgresql but heroku giving postgres in the database url

--- backend/db/session.py
+++ backend/db/session.py
@@ -8,10 +8,11 @@ from dotenv import load_dotenv
 load_dotenv()  # Load from .env file
 
 # Example PostgreSQL URL: postgresql://user:password@localhost/dbname
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "")
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL, echo=True)
-
 # Thread-safe session
 SessionLocal = scoped_session(
     sessionmaker(autocommit=False, autoflush=False, bind=engine)
